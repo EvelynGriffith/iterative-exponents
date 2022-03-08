671548bc30990143b91828eda909d806691e7ac9
EvelynGriffith <griffith01@allegheny.edu>
2022-03-08 16:11:53 -0500

    first commit

--- iterator/iterator/display.py
+++ iterator/iterator/display.py
@@ -5,15 +5,22 @@ from typing import List
 
 def convert_bool_to_answer(argument: bool):
     """Return a string-based and human-readable representation of a bool."""
-    # TODO: Return a string-based and human-readable version of a bool
-    # TODO: For instance, if argument is False then return "No"
-    # TODO: For instance, if argument is True then return "Yes"
+    # Return a string-based and human-readable version of a bool
+    if argument is True:
+        return "Yes"
+    else:
+        return "No"
+    # For instance, if argument is False then return "No"
+    # For instance, if argument is True then return "Yes"
+
 
 
 def display_list(values: List, indent=""):
     """Display the provided list when iterating and printing every indented value."""
-    # TODO: Iterate through all of the values inside of the list,
+    # iterate through all of the values inside of the list,
     # displaying them in the following fashion as an example:
+    for index in range(len(values)):
+        print(f"{indent}2**{index} = {values[index]}")
     # 2**0 = 4
     # 2**1 = 8
     # 2**2 = 16
@@ -22,5 +29,5 @@ def display_list(values: List, indent=""):
     # 2**5 = 128
     # 2**6 = 256
     # 2**7 = 512
-    # TODO: ensure that the output is indented by the numbers of spaces in the ident
-    # TODO: the display should be of the format: {indent}2**{index} = {value}
+    # ensure that the output is indented by the numbers of spaces in the ident
+    # the display should be of the format: {indent}2**{index} = {value}
--- iterator/iterator/iterate.py
+++ iterator/iterator/iterate.py
@@ -4,18 +4,26 @@
 def calculate_powers_of_two_for_loop(minimum: int, maximum: int):
     """Calculate and return the powers of 2 from an inclusive minimum to an exclusive maximum."""
     powers_list = []
-    # TODO: Leveraging your notes, the course slides, and your notes,
+    # Leveraging your notes, the course slides, and your notes,
     # please provide a complete implementation of this function that passes
     # the test suite and produces the correct output
-    # TODO: Make sure that this function performs the computation with a while loop
+    for i in range(minimum, maximum):
+        number = i**2
+        powers_list.append(number)
+    # Make sure that this function performs the computation with a for loop
     return powers_list
 
 
 def calculate_powers_of_two_while_loop(minimum: int, maximum: int):
     """Calculate and return the powers of 2 from an inclusive minimum to an exclusive maximum."""
     powers_list = []
-    # TODO: Leveraging your notes, the course slides, and your notes,
+    i = minimum
+    while i < maximum:
+        number = i**2
+        powers_list.append(number)
+        i += 1
+    # Leveraging your notes, and the course slides,
     # please provide a complete implementation of this function that passes
     # the test suite and produces the correct output
-    # TODO: Make sure that this function performs the computation with a for loop
+    # Make sure that this function performs the computation with a while loop
     return powers_list
